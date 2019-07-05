e3d0be4be4690a97f76b36e8cbcc3b899d6df709
SergeyGluxov <gluxov-serega@mail.ru>
2019-07-06 00:25:07 +0200

    Добавил adminLTE

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/home', 'CarController@addCar')->name('addCar');
+Route::get('/admin', function ()
+{
+  return view('admin');
+});
