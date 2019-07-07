f0ff6fb6f98e1074184560466354e2490dd661ca
SergeyGluxov <gluxov-serega@mail.ru>
2019-07-07 13:38:19 +0200

    Созданы команды, для создание админки

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CreateAdminPanel;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\CreateAdminPanel::class//Регистрация созданной команды
     ];
 
     /**
