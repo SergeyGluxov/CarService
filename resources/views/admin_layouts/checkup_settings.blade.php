33d0a05ed99d8a31b5b66c6da5a2f764129b11bb
SergeyGluxov <gluxov-serega@mail.ru>
2019-07-16 11:26:02 +0200

    Таблица на осмотр*

--- resources/views/admin_layouts/checkup_settings.blade.php
+++ resources/views/admin_layouts/checkup_settings.blade.php
@@ -5,7 +5,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">Таблица пользователей</h3>
+                        <h3 class="card-title">Таблица записей на осмотр</h3>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
