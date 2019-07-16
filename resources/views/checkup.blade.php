7a21433bf2c4fe541abdf61b2059f67e3628b408
PavelMalchukov <pmalchukov@mail.ru>
2019-07-16 18:42:46 +0700

    Merge branch 'feature/checkup' of C:\Users\Neo\Desktop\prog\OSPanel\domains\CarService with conflicts.

--- resources/views/checkup.blade.php
+++ resources/views/checkup.blade.php
@@ -1,4 +1,25 @@
 @extends('layouts.app')
 @section('content')
-<form-checkup-component></form-checkup-component>
+    <passport-component></passport-component>
 @endsection
+
+<?php
+if(isset($_GET["send"]))
+{
+    send_mail();
+
+    $msg_box = "<span style='color: green;'>Сообщение успешно отправлено!</span>";
+}else{
+}
+
+?>
+
+</head>
+    <body>
+    <form action="http://127.0.0.1:8000/admin/users_settings" method="get">
+        <p>Число:<br><input type="date" name="date"/> </p>
+        <input type="Submit" name="send" value="Записаться" />
+
+
+    </form>
+    </body>
