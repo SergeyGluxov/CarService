04923ff14391718fbcb542556806f5bcc95af706
Sergey <gluxov-serega@mail.ru>
2020-04-04 13:28:58 +0700

    04.04.2020

---
+++ database/migrations/2020_04_03_020119_create_services_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * title - название услуги
+     * cost - цена услуги
+     * duration - время выполнения
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('cost');
+            $table->integer('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
