10ab6b79c48e1f6342b39c516e8f6d6c36ca64e4
VagonOTT <79830156+VagonOTT@users.noreply.github.com>
2021-05-30 15:07:45 +0700

    upd

--- app/Http/Controllers/Api/Orders/OrderController.php
+++ app/Http/Controllers/Api/Orders/OrderController.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\Api\Orders;
-
-
 use App\Http\Controllers\Controller;
 use App\Repositories\OrderRepository;
 use Illuminate\Http\Request;
@@ -16,21 +14,30 @@ class OrderController extends Controller
         $this->orderRepository = $orderRepository;
     }
 
+    //получить все заказы
     public function index()
     {
         return $this->orderRepository->all();
     }
 
+    //найти определенный заказ по его ID
     public function show($id)
     {
         return $this->orderRepository->find($id);
     }
 
+    //создать новый заказ
     public function store(Request $request)
     {
         return $this->orderRepository->store($request);
     }
 
+    public function update(Request $request)
+    {
+        return $this->orderRepository->update($request);
+    }
+
+    //удалить заказ по его ID
     public function destroy($id)
     {
         return $this->orderRepository->destroy($id);
