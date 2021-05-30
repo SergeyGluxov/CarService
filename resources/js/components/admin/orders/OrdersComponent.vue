5d5dbe030b7427514839d86fb4a7607c9d091064
VagonOTT <79830156+VagonOTT@users.noreply.github.com>
2021-05-30 16:53:59 +0700

    upd

--- resources/js/components/admin/orders/OrdersComponent.vue
+++ resources/js/components/admin/orders/OrdersComponent.vue
@@ -1,13 +1,9 @@
-//Тут есть пример как использовать api в запросе денных через axios
 <template>
     <div class="container">
         <div class="row">
             <div class="col-12">
                 <h2>Форма создания новой заявки поставщику</h2>
-
-
                 <form class="form-horizontal">
-
                     <div class="form-group col-md-10">
                         <label class="control-label" for="exampleSelectDetail">Выберите деталь:</label>
                         <div>
@@ -19,7 +15,6 @@
                             </select>
                         </div>
                     </div>
-
                     <div class="form-group col-md-10">
                         <label class="control-label" for="exampleSelectBrand">Выберите поставщика:</label>
                         <div>
@@ -31,7 +26,6 @@
                             </select>
                         </div>
                     </div>
-
                     <div class="form-group  col-md-3">
                         <label class="control-label" for="count">Количество:</label>
                         <div>
@@ -39,8 +33,6 @@
                                    placeholder="1"/>
                         </div>
                     </div>
-
-
                     <div class="form-group">
                         <div class="col-md-10">
                             <button type="button" @click="storeOrder()" class="btn btn-success">
@@ -49,14 +41,11 @@
                         </div>
                     </div>
                 </form>
-
                 <hr/>
                 <h2>Заявки поставщикам</h2>
-
                 <div class="card">
                     <div class="card-header">
                     </div>
-                    <!-- /.card-header -->
                     <div class="card-body">
                         <table id="example2" class="table table-bordered table-hover">
                             <thead>
@@ -85,116 +74,6 @@
                             </tbody>
                         </table>
                     </div>
-                    <!-- /.card-body -->
-                </div>
-                <!-- /.card -->
-            </div>
-
-            <div v-if="showModal">
-                <div class="modal fade-in" style="display: block;">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" @click="showModal=false">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                                <h4 class="modal-title">Новая пользователь</h4>
-                            </div>
-                            <div class="modal-body">
-                                <form class="form-horizontal">
-                                    <div class="form-group">
-                                        <label class="control-label col-xs-3" for="createName">Введите имя:</label>
-                                        <div class="col-xs-9">
-                                            <input type="text" class="form-control" v-model="createName"
-                                                   id="createName"
-                                                   placeholder="Иванов Иван Иванович"/>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-xs-3" for="createName">Введите номер
-                                            телефона:</label>
-                                        <div class="col-xs-9">
-                                            <input type="text" class="form-control" v-model="createPhone"
-                                                   id="createPhone"
-                                                   placeholder="+79505728020"/>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-xs-3" for="createEmail">Введите
-                                            эл.почту:</label>
-                                        <div class="col-xs-9">
-                                            <input type="text" class="form-control" v-model="createEmail"
-                                                   id="createEmail"
-                                                   placeholder="ivanov.ii@gmail.com"/>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-xs-3" for="createPassword">Введите
-                                            пароль:</label>
-                                        <div class="col-xs-9">
-                                            <input type="text" class="form-control" v-model="createPassword"
-                                                   id="createPassword"
-                                                   placeholder="*****"/>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label class="control-label col-xs-3" for="createMarkCar">Марка и модель
-                                            авто:</label>
-                                        <div class="col-xs-4">
-                                            <input type="text" class="form-control" v-model="createMarkCar"
-                                                   id="createMarkCar"
-                                                   placeholder="Nissan"/>
-                                        </div>
-                                        <div class="col-xs-4">
-                                            <input type="text" class="form-control" v-model="createModelCar"
-                                                   id="createModelCar"
-                                                   placeholder="Patrol"/>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label class="control-label col-xs-3" for="createGosNumberCar">Введите гос.
-                                            номер:</label>
-                                        <div class="col-xs-9">
-                                            <input type="text" class="form-control" v-model="createGosNumberCar"
-                                                   id="createGosNumberCar"
-                                                   placeholder="X001A142"/>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <div class="col-xs-offset-6 col-xs-9">
-                                            <button type="button" @click="store" class="btn btn-success">Создать
-                                                пользователя
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div v-if="showModalImport">
-            <div class="modal fade-in" style="display: block; padding-right: 17px;">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" @click="showModalImport=false">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            <h4 class="modal-title">Импорт пользователей из Excel</h4>
-                        </div>
-                        <div class="modal-body">
-                            <form class="form" method="POST" enctype="multipart/form-data"
-                                  action="/admin/users/import">
-                                <input type="file" id="excelUploadUsers" name="excelUploadUsers">
-                                <br>
-                                <button type="submit" class="btn btn-primary">Импорт из файла</button>
-                            </form>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
@@ -202,62 +81,49 @@
 </template>
 
 <script>
-    import moment from "moment";
-
     export default {
         data: function () {
             return {
                 orders: [],
-                showModal: false,
-                //UserData
-                createName: '',
-                createEmail: '',
-                createPassword: '',
-                createPhone: '',
-                createMarkCar: '',
-                createModelCar: '',
-                createGosNumberCar: '',
+                details: [],
+                suppliers: [],
                 vmDetail: '',
                 vmSupplier: '',
                 vmCount: '',
-                details: [],
-                suppliers: [],
                 detailSelected:'',
                 supplierSelected:'',
-
-                showModalImport: false,
             }
         },
         mounted() {
             this.update();
             this.getDetails();
             this.getSuppliers();
-
         },
         methods: {
-            newModal: function () {
-                $('#addNew').modal('show');
-            },
+            //Получить все заказы
             update: function () {
                 axios.get('/orders').then((response) => {
                     this.orders = response.data;
                     console.log(response.data);
                 });
             },
+            //Получить все детали
             getDetails: function () {
                 axios.get('/details').then((response) => {
                     this.details = response.data;
                     console.log(response.data);
                 });
             },
-
+            //Получить всех поставщиков
             getSuppliers: function () {
                 axios.get('/suppliers').then((response) => {
                     this.suppliers = response.data;
                     console.log(response.data);
                 });
             },
+            //Отправить запрос на создание нового заказа
             storeOrder: function () {
+                //Формируем данные для запроса
                 var formData = new FormData();
                 formData.append('status', 'create');
                 formData.append('detail_id', this.detailSelected);
@@ -277,14 +143,13 @@
                         }
                     });
             },
+            //Удалить заказ
             deleteOrder: function (id) {
                 axios.delete('/orders/' + id).then((response) => {
                     this.update();
                 });
             },
-            convertDat: function (date) {
-                return moment(date).format('DD.MM.YYYY')
-            },
+
             onSelectDetail(e) {
                 this.detailSelected = e.target.value;
             },
